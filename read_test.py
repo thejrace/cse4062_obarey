00bd0d41849cd9aff353dc0b48df5a395e920a62
Obarey <ahmetkanbur@gmail.com>
2020-05-06 00:02:24 +0300

    cumulative updated

---
+++ read_test.py
@@ -0,0 +1,11 @@
+import csv
+
+with open("data/data_origin.DUMP", encoding="utf8") as tsv:
+    counter = 0
+    for line in csv.reader(tsv, dialect="excel-tab"):
+        counter += 1
+
+        print(line[2])
+
+        if counter > 10:
+            break
